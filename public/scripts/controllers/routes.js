8418e4af0b263a105ee58b47468a6c3704a5cf1f
andyfiveeleven <fouracesguitar@gmail.com>
2018-04-04 17:23:32 -0700

    scaffold and todo's on nav.js and index.html. page.js  routes build

---
+++ public/scripts/controllers/routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+page('/', app.pageView.home);
+page('/home', app.pageView.home);
+page('/portfolio', app.pageView.portfolio);
+page('/resume', app.pageView.resume);
+page('/about', app.pageView.about);
+page('/contact', app.pageView.contact);
+page();
---
+++ public/scripts/views/nav.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var app = app || {};
+
+(function(module){
+  var pageView = {};
+    //TODO fill out for the rest of the routes in the nav. hide show wiht jquery 
+  pageView.about = () => {
+    //TODO fill with jquery hides and shows related to classes
+  }
+})
