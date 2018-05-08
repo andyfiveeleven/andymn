988e32b0c4d98ab4ff5378c54da75e13184c50a3
andyfiveeleven <fouracesguitar@gmail.com>
2018-05-08 13:31:38 -0700

    resume styling finished hamburger

--- public/scripts/views/nav-display.js
+++ public/scripts/views/nav-display.js
@@ -1,10 +1,12 @@
 $('.nav-icon').click(function(){
+  $('.nav-icon').toggleClass('open');
   $('body').toggleClass('hidden');
   $('.main-nav ul').toggleClass('menu');
   $('.main-nav ul').toggleClass('display-nav');
 });
 
 $('.main-nav ul li').click(function(){
+  $('.nav-icon').toggleClass('open');
   $('body').removeClass('hidden');
   $('.main-nav ul').addClass('menu');
   $('.main-nav ul').removeClass('display-nav');
