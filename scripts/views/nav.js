77a5c27883ff1f06d2aedd56a6fc44f7df5964f4
andyfiveeleven <fouracesguitar@gmail.com>
2018-06-21 12:24:33 -0700

    reordering the file system for use in trasmit

--- scripts/views/nav.js
+++ scripts/views/nav.js
@@ -8,30 +8,35 @@ var app = app || {};
   pageView.about = () => {
     $('.tab-content').hide();
     $('.about').show();
+    $('html,body').scrollTop(0);
   };
 
   pageView.home = () => {
     //TODO fill with jquery hides and shows related to classes
     $('.tab-content').hide();
     $('.home').show();
+    $('html,body').scrollTop(0);
   };
 
   pageView.resume = () => {
     //TODO fill with jquery hides and shows related to classes
     $('.tab-content').hide();
     $('.resume').show();
+    $('html,body').scrollTop(0);
   };
 
   pageView.portfolio = () => {
     //TODO fill with jquery hides and shows related to classes
     $('.tab-content').hide();
     $('.portfolio').show();
+    $('html,body').scrollTop(0);
   };
 
   pageView.contact = () => {
     //TODO fill with jquery hides and shows related to classes
     $('.tab-content').hide();
     $('.contact').show();
+    $('html,body').scrollTop(0);
   };
   module.pageView = pageView;
 }(app));
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static('./'));
 
 app.get('*', function(req, res){
-  res.sendFile('index.html', {root: './public/'})
+  res.sendFile('index.html', {root: './'})
 });
 
 app.listen(PORT, () => console.log(`You are on port: ${PORT}`));
